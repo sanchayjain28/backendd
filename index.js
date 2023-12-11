2099aa87e80f456937625cd3da16224ed21a9fc0
sanchayjain28 <sanchayjain.it24@jecrc.ac.in>
2023-12-11 17:16:10 +0530

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello orld!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send("snachaytwu=itter")
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
